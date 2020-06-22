cd76eba302172b4c2e9ba40b3bc91e91ce7242a6
Bogdan1707 <33845200+Bogdan1707@users.noreply.github.com>
2020-06-22 14:33:33 +0300

    Test commit2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ API_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 requests_payload = {
     'chat_id': 380314062, #dfddfdfdfdfd
     'text': 'Hello', #dfdfdfdlhjlkhl
-    'reply_to_message_id': 4
+    'reply_to_message_id': 4 #comit
 }
 
 r = requests.get(API_URL, data=requests_payload)
