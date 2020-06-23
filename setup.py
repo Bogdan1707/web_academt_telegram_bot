ec59dffabf43f858a2a9d2e50da73f9953675427
Bogdan1707 <33845200+Bogdan1707@users.noreply.github.com>
2020-06-23 15:31:26 +0300

    Delete dscr

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import requests
 TOKEN = '1228052546:AAHijJi0TFuUJ-csaPU459-XNT3KY_fk28A'
 API_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 requests_payload = {
-    'chat_id': 380314062, #dfddfdfdfdfd
-    'text': 'Hello', #dfdfdfdlhjlkhl
-    'reply_to_message_id': 4 #comitds
+    'chat_id': 380314062,
+    'text': 'Hello',
+    'reply_to_message_id': 4 
 }
 
 r = requests.get(API_URL, data=requests_payload)
